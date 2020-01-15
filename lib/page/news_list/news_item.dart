3ed350e3a7b48a28e3d375222a1ee47e839430e0
BaoZhou <600230bz>
2020-01-16 01:26:49 +0800

    添加脚本

--- lib/page/news_list/news_item.dart
+++ lib/page/news_list/news_item.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/cupertino.dart';
-
 import 'news_list_entity.dart';
+import 'package:flutter_wanandroid/res/styles.dart';
+import 'package:html_unescape/html_unescape.dart';
 
-
+/// 文章列表Item
+///
+/// author : BaoZhou
+/// date : 2020-1-16 1:09
 class NewsListWidget extends StatefulWidget {
   final NewsBean itemBean;
+
   NewsListWidget(this.itemBean);
 
   @override
@@ -14,8 +19,25 @@ class NewsListWidget extends StatefulWidget {
 class _NewsListWidgetState extends State<NewsListWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(widget.itemBean.title),
+    return Column(
+        children: <Widget>[
+            Expanded(
+                flex: 1,
+                child: Container(
+                  child: Text(HtmlUnescape().convert(widget.itemBean.title),
+                    style: TextStyles.textBold18,
+                    maxLines: 3),
+                )
+    ),
+          Expanded(
+            flex: 2,
+              child: Container(
+                child: Text(HtmlUnescape().convert(widget.itemBean.title),
+                    style: TextStyles.textBold18,
+                    maxLines: 3),
+              )
+          )
+        ],
     );
   }
 }
